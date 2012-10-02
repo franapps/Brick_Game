b5afac991516bc2a1160735a45181ae4ca73a0bb
Francis Apaloo <frapweb12@gmail.com>
2012-10-02 17:00:00 +0100

    Added scoring and lives.
    velocity changed to ballSpeed
    added paddle speed
    added score variable
    added lives variable
    added score increase when brick is destroyed.

--- brick.py
+++ brick.py
@@ -33,7 +33,10 @@ ball = pygame.Rect(int(SCREENWIDTH/2), int(SCREENHEIGHT/2), BALLWIDTH, BALLWIDTH
 # Handle ball movement and animation
 horizontal = 5
 vertical = 5
-velocity = [horizontal, vertical]
+ballSpeed = [horizontal, vertical]
+
+# Handle paddle horizontal movement (the paddle will never move vertically)
+paddleSpeed = 5
 
 # Setup the bricks
 WIDTH = 50
@@ -46,6 +49,13 @@ def drawLevel():
 		for j in range(3, 11):
 			newBrick = [pygame.Rect((i * WIDTH), (j * HEIGHT), WIDTH, HEIGHT), color[(random.randint(1, 5)-1)], None]
 			bricks.append(newBrick)
+
+# Function to check if the ball has hit the paddle - Under construction
+def ballHasHitPaddle(ball, paddle):
+	if ball.bottom == paddle.top:
+		return True
+	else:
+		return False
 
 # Function to check if the ball has hit a brick.
 def ballHasHitBrick(ball, b):
@@ -69,11 +79,14 @@ if __name__ == '__main__':
 				Run = False
 
 		# Ball bounces off the walls and ceiling, player loses a life when the ball hits the bottom of the screen.
-		ball = ball.move(velocity)
+		ball = ball.move(ballSpeed)
 		if ball.left < 0 or ball.right > SCREENWIDTH:
-			velocity[0] = -velocity[0]
-		if ball.top < 0 or ball.bottom > SCREENHEIGHT:
-			velocity[1] = -velocity[1]
+			ballSpeed[0] = -ballSpeed[0]
+		if ball.top < 0:
+			ballSpeed[1] = -ballSpeed[1]
+		if ball.bottom > SCREENHEIGHT:
+			ballSpeed[1] = -ballSpeed[1]
+			lives -= 1
 
 		screen.fill((20,50,150))
 
@@ -81,11 +94,16 @@ if __name__ == '__main__':
 		for b in bricks:
 			if ballHasHitBrick(ball, b):
 				if b[0].right == ball.left or b[0].left == ball.right:
-					velocity[0] = -velocity[0]
+					ballSpeed[0] = -ballSpeed[0]
 				if b[0].bottom == ball.top or b[0].top == ball.bottom:
-					velocity[1] = -velocity[1]
+					ballSpeed[1] = -ballSpeed[1]
 				bricks.remove(b)
+				score += 100
 			pygame.draw.rect(screen, b[1], b[0])
+
+		# Every 5000 points the player gains an extra life
+		if score != 0 and score % 5000 == 0:
+			lives += 1
 
 		pygame.draw.rect(screen, WHITE, ball)
 		pygame.draw.rect(screen, BLACK, player)
---
+++ readme.txt
@@ -0,0 +1,5 @@
+First project by Francis Apaloo
+-------------------------------
+
+Re-build of classic brick/breakout games as a first project using Python 3.2 
++ Pygame extension.
